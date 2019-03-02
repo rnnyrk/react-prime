71a9194e81bb419077b73bf61b35f4c8e81d9775
Sander Vispoel <sandervispoel@hotmail.com>
2019-03-02 18:38:15 +0100

    Add Omit functional type

--- src/types/general.ts
+++ src/types/general.ts
@@ -3,3 +3,8 @@ import { RouteComponentProps as IRouteComponentProps } from 'react-router-dom';
 
 // Make generics optional
 export type RouteComponentProps<P = any, C = any> = IRouteComponentProps<P, C>;
+
+// Exclude K from T
+// T = Type
+// K = Props to be excluded from T
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
