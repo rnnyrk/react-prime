c2a993fd6929e3642ed8707b6905b429dfc893e9
Ronny <ronny.rook@labela.nl>
2019-05-08 10:36:51 +0200

    Fix issue with devServer types

--- config/webpack.config.dev.ts
+++ config/webpack.config.dev.ts
@@ -1,6 +1,6 @@
-import * as path from 'path';
-import * as webpack from 'webpack';
-import * as devServer from 'webpack-dev-server';
+import path from 'path';
+import webpack from 'webpack';
+import devServer from 'webpack-dev-server';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import globals from '../config/globals';
 import { merge } from './webpack.config.base';
@@ -20,7 +20,7 @@ const devConfig: devServer.Configuration = merge({
   ],
   devServer: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: Number(process.env.PORT) || 3000,
     hot: true,
     stats: 'minimal',
     historyApiFallback: true,
